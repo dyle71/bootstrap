e82389c8649a98f9207f0506aebae69fd8f959ef
Oliver Maurhart <dyle71@gmail.com>
2024-09-02 11:39:51 +0200

    Add installtion script for zsh/omz/powerlevel10k

---
+++ zsh.apt.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+apt-get install -y zsh wget
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
