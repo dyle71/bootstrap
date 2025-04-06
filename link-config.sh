4189476f08d170a6693a5158dfabd6db073b7daf
Oliver Maurhart <dyle71@gmail.com>
2025-04-06 15:23:17 +0200

    Fix typo in gitconfig symlink path in link-config.sh

--- link-config.sh
+++ link-config.sh
@@ -8,6 +8,6 @@ mkdir -p ${HOME}/.config &> /dev/null
 ln -sfv ${INSTALL_PATH}/home/.zshrc ${HOME}/.zshrc
 ln -sfv ${INSTALL_PATH}/home/.zshrc.dyle ${HOME}/.zshrc.dyle
 ln -sfv ${INSTALL_PATH}/home/.p10k.zsh ${HOME}/.p10k.zsh
-ln -sfv ${INSTALL_PATH}/home/.gitconfig ${HOME}.gitconfig
+ln -sfv ${INSTALL_PATH}/home/.gitconfig ${HOME}/.gitconfig
 ln -sfv ${INSTALL_PATH}/home/.config/nvim ${HOME}/.config/nvim
 ln -sfv ${INSTALL_PATH}/home/.config/bat ${HOME}/.config/bat
