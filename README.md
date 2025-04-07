481982d899f5175f5cd1d82fb68907779b80c3a5
Oliver Maurhart <dyle71@gmail.com>
2025-04-07 18:32:49 +0200

    Update README and refine symlinks in link-config.sh for improved setup

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # bootstrap
 
 Bootstrap my ideal Linux shell environment
-
-Invoke `bootstrap.apt.sh` (Debian/Ubuntu) or `bootstrap.pacman.sh` (Arch) to
-install and configure the essential apps.
-
-Blabalb
+Invoke `bootstrap.sh` install and configure the essential apps.
 
 ---  
 
--- link-config.sh
+++ link-config.sh
@@ -4,10 +4,12 @@ INSTALL_PATH=$(dirname $(readlink -f $0))
 
 cd ${HOME}
 mkdir -p ${HOME}/.config &> /dev/null
+mkdir -p ${HOME}/.config/bat &> /dev/null
+mkdir -p ${HOME}/.config/nvim &> /dev/null
 
 ln -sfv ${INSTALL_PATH}/home/.zshrc ${HOME}/.zshrc
 ln -sfv ${INSTALL_PATH}/home/.zshrc.dyle ${HOME}/.zshrc.dyle
 ln -sfv ${INSTALL_PATH}/home/.p10k.zsh ${HOME}/.p10k.zsh
 ln -sfv ${INSTALL_PATH}/home/.gitconfig ${HOME}/.gitconfig
-ln -sfv ${INSTALL_PATH}/home/.config/nvim ${HOME}/.config/nvim
-ln -sfv ${INSTALL_PATH}/home/.config/bat ${HOME}/.config/bat
+ln -sfv ${INSTALL_PATH}/home/.config/bat/config ${HOME}/.config/bat/config
+ln -sfv ${INSTALL_PATH}/home/.config/nvim/init.lua ${HOME}/.config/nvim/init.lua
