cfcfd208a46c24fb4548c0f9e96a25614470f290
Oliver Maurhart <dyle71@gmail.com>
2025-04-08 09:55:19 +0200

    Enhance bootstrap script to dynamically install applications based on OS distribution

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,24 +6,35 @@ if [[ "$(basename ${0})" != "bootstrap.sh" ]]; then
 fi
 cd $(dirname $(readlink -f $0))
 
-ARCH_APPS="neovim git net-tools eza iproute2 bat zsh curl wget"
-DEB_APPS="neovim git net-tools exa iproute2 batcat zsh curl wget"
+[[ -f /etc/os-release ]] && . /etc/os-release
+OS_DIST_NAME=${ID}
+
+APPS_TO_INSTALL=""
+if [[ "${OS_DIST_NAME}" == "arch" ]]; then
+  APPS_TO_INSTALL="neovim git net-tools eza iproute2 bat zsh curl wget"
+elif [[ "${OS_DIST_NAME}" == "ubuntu" ]]; then
+  APPS_TO_INSTALL="neovim git net-tools exa iproute2 bat zsh curl wget"
+elif [[ "${OS_DIST_NAME}" == "debian" ]]; then
+  APPS_TO_INSTALL="neovim git net-tools exa iproute2 bat zsh curl wget"
+else
+  echo "Unsupported OS distribution: ${OS_DIST_NAME}"
+  exit 1
+fi
 
 IS_APT_GET=$(command -v apt-get)
 IS_PACMAN=$(command -v pacman)
-
 if [[ -z "${IS_APT_GET}" && -z "${IS_PACMAN}" ]]; then
   echo "Neither apt-get nor pacman found. Cannot proceed..."
   exit 1
 fi
 
 if [[ -n "${IS_APT_GET}" ]]; then
-  echo "sudo apt-get install -y ${DEB_APPS}"
-  sudo apt-get install -y ${DEB_APPS}
+  echo "sudo apt-get install -y ${APPS_TO_INSTALL}"
+  sudo apt-get install -y ${APPS_TO_INSTALL}
 fi
 if [[ -n "${IS_PACMAN}" ]]; then
-  echo "sudo pacman -S --noconfirm ${ARCH_APPS}"
-  sudo pacman -S ${ARCH_APPS}
+  echo "sudo pacman -S --noconfirm ${APPS_TO_INSTALL}"
+  sudo pacman -S ${APPS_TO_INSTALL}
 fi
 
 if [[ -z "${HOME}" ]]; then
